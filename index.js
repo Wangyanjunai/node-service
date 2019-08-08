04cc296281970ec733b91dd4c1ef31d0a168f71b
wangyanjun <406151651@qq.com>
2019-08-08 15:22:00 +0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const http = require('http');
+const url = require('url');
+
+const server = http.createServer(function (req, res) {
+    const pathname = url.parse(req.url).pathname;
+    res.writeHead(200, {'Content-Type': 'application/json; charset=utf-8'});
+    if (pathname === '/') {
+        res.end(JSON.stringify({"index": "欢迎来到首页"}));
+    } else if (pathname === '/health' || pathname === '/health.json') {
+        res.end(JSON.stringify({"status": "UP"}));
+    } else {
+        res.end("404");
+    }
+});
+server.listen(8060, function () {
+    console.log("listening on localhost 8060");
+})
